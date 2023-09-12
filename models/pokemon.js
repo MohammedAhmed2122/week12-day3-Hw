22db08ef2eb663a31ed64c3d617867e25a205787
Mohammed Ahemd <moealwali@Mohammeds-MacBook-Pro.local>
2023-09-11 22:53:04 -0500

    update

---
+++ models/pokemon.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const pokemonSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    img: {type: String, required: true }
+}, {
+    timestamps: true
+})
+
+const Pokemon = mongoose.model('Pokemon', pokemonSchema)
+
+module.exports = Pokemon
+
+
+
+
+
